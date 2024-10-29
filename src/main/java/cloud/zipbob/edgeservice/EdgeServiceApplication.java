81656b55d04f007d1e274e2b35208d99d92bc347
Kyumin-Chang <kyumin0529@naver.com>
2024-10-29 21:07:49 +0900

    feat: Basic Config

--- src/main/java/cloud/zipbob/edgeservice/EdgeServiceApplication.java
+++ src/main/java/cloud/zipbob/edgeservice/EdgeServiceApplication.java
@@ -2,8 +2,11 @@ package cloud.zipbob.edgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class EdgeServiceApplication {
 
     public static void main(String[] args) {
