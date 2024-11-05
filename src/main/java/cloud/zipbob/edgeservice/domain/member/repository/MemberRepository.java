b729757bbfc66ecefb1088a44a2b715be537fb25
Kyumin-Chang <kyumin0529@naver.com>
2024-11-06 01:10:17 +0900

    chore: Repository 어노테이션 추가

--- src/main/java/cloud/zipbob/edgeservice/domain/member/repository/MemberRepository.java
+++ src/main/java/cloud/zipbob/edgeservice/domain/member/repository/MemberRepository.java
@@ -3,9 +3,11 @@ package cloud.zipbob.edgeservice.domain.member.repository;
 import cloud.zipbob.edgeservice.domain.member.Member;
 import cloud.zipbob.edgeservice.oauth2.SocialType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
