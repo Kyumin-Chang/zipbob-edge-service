b964b6994562999d159d9da268d6e84d844e7776
Kyumin-Chang <kyumin0529@naver.com>
2024-12-20 21:00:03 +0900

    chore: 테스트를 위한 이메일 서비스 임시 중단

--- src/main/java/cloud/zipbob/edgeservice/domain/member/service/MemberServiceImpl.java
+++ src/main/java/cloud/zipbob/edgeservice/domain/member/service/MemberServiceImpl.java
@@ -16,7 +16,6 @@ import cloud.zipbob.edgeservice.domain.member.response.*;
 import cloud.zipbob.edgeservice.global.redis.RedisService;
 import cloud.zipbob.edgeservice.oauth2.SocialType;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,8 +24,6 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
-// TODO 관리자 전용 api 제작하기 (멤버 삭제)
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
     private final JwtTokenProvider jwtTokenProvider;
